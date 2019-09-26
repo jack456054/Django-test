01f2b43d5c5e52c284188b43b3e378d520925be9
jack456054 <jack456054@hotmail.com>
2019-09-26 13:00:00 +0800

    Populate fake data

--- first_project/first_app/admin.py
+++ first_project/first_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from first_app.models import AccessRecode, Topic, Webpage
 # Register your models here.
+admin.site.register(AccessRecode)
+admin.site.register(Topic)
+admin.site.register(Webpage)

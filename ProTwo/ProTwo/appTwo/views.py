0cee63cc4c61e6ca67a081694050e69c5882051b
jack456054 <jack456054@hotmail.com>
2019-09-27 14:18:31 +0800

    Registration web

--- ProTwo/ProTwo/appTwo/views.py
+++ ProTwo/ProTwo/appTwo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from appTwo.models import User
+# from appTwo.models import User
+from appTwo.forms import NewUserForm
 
 
 # Create your views here.
@@ -8,6 +9,17 @@ def index(request):
 
 
 def users(request):
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users': user_list}
-    return render(request, 'appTwo/users.html', context=user_dict)
+    # user_list = User.objects.order_by('first_name')
+    # user_dict = {'users': user_list}
+    # return render(request, 'appTwo/users.html', context=user_dict)
+
+    form = NewUserForm()
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('ERROR FORM INVALID')
+
+    return render(request, 'appTwo/users.html', {'form': form})
